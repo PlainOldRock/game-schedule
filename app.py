cefd32e62387cc9af94bcfb35854d1fc2af100a5
plainoldrock <connor.laskodi@gmail.com>
2025-05-24 15:05:43 -0400

    fixed dict

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ if user_info is not None:
         st.toast("Save your changes with 'Save Events'!")
     elif state["callback"] == 'eventClick':
         st.write(state)
-        edit_event(state,state["callback"]["eventClick"]["id"])
+        edit_event(state,state["eventClick"]["id"])
     if st.button("Save Events"):
         with open("data.json", "w") as fo:
             json.dump(st.session_state['events'], fo)
