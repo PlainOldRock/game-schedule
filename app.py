875763c1cf96b66463619d7278965f1e3d242c85
plainoldrock <connor.laskodi@gmail.com>
2025-05-24 23:31:48 -0400

    Testing Background colros for it

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.title("Gaming Week Part 2 Schedule")
 def update_colors(username : str, color):
     for event in st.session_state["events"]:
         if event["user"] == username:
-            event["eventColor"] = color
+            event["backgroundColor"] = color
 
 @st.dialog("Settings")
 def user_setting():
