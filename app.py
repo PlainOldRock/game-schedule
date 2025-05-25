11d360d7962044f93877146f7b804d64328dd2c4
plainoldrock <connor.laskodi@gmail.com>
2025-05-25 00:01:16 -0400

    Shopping

--- app.py
+++ app.py
@@ -159,8 +159,9 @@ def get_new_id():
 def check_today_entries():
     num_entry = 0
     global user_info
-    #print(f"{event["created"]} == {date.today()}")
+    
     for event in st.session_state["events"].values():
+        print(f"{event["created"]} == {date.today()}")
         if user_info["username"] == event["user"]:
             if event["created"] == date.today():
                 num_entry += 1
