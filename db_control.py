ba69d8ce2c77d7c6a8cf118c0c4a453442c37038
PlainOldRock <connor.laskodi@gmail.com>
2025-05-26 00:48:09 -0400

    updated URL

--- db_control.py
+++ db_control.py
@@ -9,7 +9,7 @@ class Db_conn:
         self.connection = None
         self.cursor = None
         self.connection = mysql.connector.connect(
-            host='plainoldrock.duckdns.org',
+            host='ultihouseserver.wampus-pickerel.ts.net',
             port="3306",
             database='DISCORD_SCHEDULE',
             user=str(db_user),
