869a9a2e9c316282ed89fcd225e61f276990ac43
plainoldrock <connor.laskodi@gmail.com>
2025-05-25 20:22:19 -0400

    w.e

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ calendar_options = {
 def get_initial_events():
     if os.path.exists("data.json"):
         with open("data.json","r") as fo:
-            dic = json.loads(fo)
+            dic = json.load(fo)
     else:
         dic = {}
     return dic
