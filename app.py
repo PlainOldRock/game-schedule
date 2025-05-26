ff3340e616814df87624f49d3781f48122c965d2
plainoldrock <connor.laskodi@gmail.com>
2025-05-25 20:15:59 -0400

    Fixed bad push

--- app.py
+++ app.py
@@ -224,15 +224,16 @@ def add_event(state):
                         break
                 if flag == False:
                     my_id = get_new_id()
-                    db_conn.add_event({
+                    st.session_state['events'][my_id] = {
                         "start": event_start,
                         "end":event_end,
                         "title": event_title + f"\n{user_info['username']}\n{event_game}",
                         "user": user_info['username'],
                         "game": event_game,
+                        "id":my_id,
                         "created":str(date.today()),
                         "backgroundColor":user_cntl.usersDB.get_user(user_info["username"])["color"]
-                    })
+                    }
                     st.rerun()
                 else:
                     st.error("Events Can't Overlap")
@@ -270,7 +271,7 @@ def add_event_button():
                     st.session_state['events'][my_id] = {
                         "start": event_start,
                         "end":event_end,
-                        "title": event_title + f"\n{user_info['username']}\n{event_game}",
+                        "title": event_title + f"\n{user_info["username"]}\n{event_game}",
                         "user": user_info['username'],
                         "game": event_game,
                         "id":my_id,
